c414c4c85fa755ffa9c43e57d7f53f4ae9ee5bc5
Betmira <betmira@gmail.com>
2018-06-03 08:08:27 +0200

    replace css to scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
-}
+export class AppComponent {}
